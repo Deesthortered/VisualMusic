f645e2dcb39016cf632f2b34e12e7366c7a3be38
Anatolii Davydko <anatoly.davydko@gmail.com>
2020-07-08 21:42:31 +0300

    GUI structure is little bit changed, File Dialog is implemented.

--- src/main/java/org/visualmusic/VisualMusicApplication.java
+++ src/main/java/org/visualmusic/VisualMusicApplication.java
@@ -1,8 +1,12 @@
 package org.visualmusic;
 
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.event.Event;
+import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
@@ -11,12 +15,14 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
+import java.io.File;
 import java.io.IOException;
 
 
-public class VisualMusicApplication extends Application {
+public class VisualMusicApplication extends Application implements EventHandler<Event> {
     public static void main(String[] args) {
         ConfigurationManager.getInstance();
         launch(args);
@@ -25,20 +31,40 @@ public class VisualMusicApplication extends Application {
     private ConfigurationManager configurationManager;
     private ApplicationService applicationService;
 
+    private Stage mainStage;
+    private Scene mainScene;
+    private Button buttonLoadFromFile;
+    private Button buttonLoadFromURL;
+    private Button buttonOpenFileDialog;
+    private TextField textPathToImageFile;
+    private TextField textURLToImageFile;
+
     private Image defaultImage;
     private Image loadedImage;
 
     @Override
     public void start(Stage primaryStage) {
-        configurationManager = ConfigurationManager.getInstance();
-        applicationService = ApplicationService.getInstance();
-        loadDefaultImage();
+        try {
+            this.mainStage = primaryStage;
+            configurationManager = ConfigurationManager.getInstance();
+            applicationService = ApplicationService.getInstance();
+            loadDefaultImage();
 
-        Scene scene = new Scene(makeMainPane(), configurationManager.getWINDOW_WIDTH(), configurationManager.getWINDOW_HEIGHT());
-        primaryStage.setScene(scene);
-        primaryStage.setTitle(configurationManager.getWINDOW_TITLE());
-        primaryStage.setResizable(false);
-        primaryStage.show();
+            this.mainScene = new Scene(makeMainPane(), configurationManager.getWINDOW_WIDTH(), configurationManager.getWINDOW_HEIGHT());
+            primaryStage.setScene(this.mainScene);
+            primaryStage.setTitle(configurationManager.getWINDOW_TITLE());
+            primaryStage.setResizable(false);
+            primaryStage.show();
+        } catch (Throwable rootException) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Root error!");
+            alert.setHeaderText("There is root error! Application will be closed.");
+            alert.setContentText("There is an error that application can't handle. \nException message: " +
+                    rootException.getMessage());
+            alert.showAndWait();
+
+            Platform.exit();
+        }
     }
 
     private void loadDefaultImage() {
@@ -47,7 +73,13 @@ public class VisualMusicApplication extends Application {
                     configurationManager.getPATH_DEFAULT_IMAGE(),
                     false);
         } catch (IOException e) {
-            e.printStackTrace();
+            defaultImage = null;
+
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Error!");
+            alert.setHeaderText("Error! Default image not found!");
+            alert.setContentText("Exception message: " + e.getMessage());
+            alert.showAndWait();
         }
     }
 
@@ -62,50 +94,38 @@ public class VisualMusicApplication extends Application {
         HBox paneSummaryBox = new HBox();
         paneSummaryBox.getChildren().addAll(paneButtonBox, paneTextFieldsBox);
 
-        Button buttonLoadFromFile = new Button("Load image from file");
+        buttonLoadFromFile = new Button("Load image from file");
         paneButtonBox.getChildren().add(buttonLoadFromFile);
         buttonLoadFromFile.setMinWidth(buttonWidth);
-        buttonLoadFromFile.pressedProperty().addListener((observable, wasPressed, pressed) -> {
-            if (wasPressed) {
-                System.out.println("wasPressed");
-            }
-        });
+        buttonLoadFromFile.setOnAction(this::handle);
 
-        Button buttonLoadFromURL = new Button("Load image from URL");
+        buttonLoadFromURL = new Button("Load image from URL");
         paneButtonBox.getChildren().add(buttonLoadFromURL);
         buttonLoadFromURL.setMinWidth(buttonWidth);
-        buttonLoadFromURL.pressedProperty().addListener((observable, wasPressed, pressed) -> {
-            if (wasPressed) {
-                System.out.println("wasPressed");
-            }
-        });
+        buttonLoadFromURL.setOnAction(this::handle);
 
         HBox boxFilePath = new HBox();
         paneTextFieldsBox.getChildren().add(boxFilePath);
 
-        Button buttonOpenFileDialog = new Button("Browse");
+        buttonOpenFileDialog = new Button("Browse");
         boxFilePath.getChildren().add(buttonOpenFileDialog);
         buttonOpenFileDialog.setMinWidth(buttonBrowseWidth);
-        buttonOpenFileDialog.pressedProperty().addListener((observable, wasPressed, pressed) -> {
-            if (wasPressed) {
-                System.out.println("wasPressed");
-            }
-        });
+        buttonOpenFileDialog.setOnAction(this::handle);
 
-        TextField textPathToImageFile = new TextField();
+        textPathToImageFile = new TextField();
         boxFilePath.getChildren().add(textPathToImageFile);
         textPathToImageFile.setMinWidth(configurationManager.getWINDOW_WIDTH() - buttonWidth - buttonBrowseWidth - 30);
         textPathToImageFile.setMaxWidth(configurationManager.getWINDOW_WIDTH() - buttonWidth - buttonBrowseWidth - 30);
 
-        TextField textURLToImageFile = new TextField();
+        textURLToImageFile = new TextField();
         paneTextFieldsBox.getChildren().add(textURLToImageFile);
         textURLToImageFile.setMinWidth(configurationManager.getWINDOW_WIDTH() - buttonWidth - 30);
         textURLToImageFile.setMaxWidth(configurationManager.getWINDOW_WIDTH() - buttonWidth - 30);
 
-        paneButtonBox.setPadding(new Insets(padding,padding,padding,padding));
-        paneTextFieldsBox.setPadding(new Insets(padding,padding,padding,0));
+        paneButtonBox.setPadding(new Insets(padding, padding, padding, padding));
+        paneTextFieldsBox.setPadding(new Insets(padding, padding, padding, 0));
 
-        paneButtonBox.setPadding(new Insets(padding,padding,padding,padding));
+        paneButtonBox.setPadding(new Insets(padding, padding, padding, padding));
 
         return paneSummaryBox;
     }
@@ -130,5 +150,41 @@ public class VisualMusicApplication extends Application {
         mainPane.getChildren().addAll(paneLoadImage, paneShowImage);
 
         return mainPane;
+    }
+
+    @Override
+    public void handle(Event event) {
+        Object source = event.getSource();
+        if (source == buttonOpenFileDialog) {
+            handleButtonOpenFileDialog();
+        } else if (source == buttonLoadFromFile) {
+            handleButtonLoadFromFile();
+        } else if (source == buttonLoadFromURL) {
+            handleButtonLoadFromURL();
+        }
+    }
+
+    public void handleButtonOpenFileDialog() {
+        FileChooser.ExtensionFilter fileImageExtensions =
+                new FileChooser.ExtensionFilter("Images", "*.jpg", "*.png");
+        FileChooser.ExtensionFilter fileAllExtensions =
+                new FileChooser.ExtensionFilter("All files", "*.*");
+
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.getExtensionFilters().add(fileImageExtensions);
+        fileChooser.getExtensionFilters().add(fileAllExtensions);
+
+        File file = fileChooser.showOpenDialog(this.mainStage);
+        if (file != null) {
+            textPathToImageFile.setText(file.toURI().toString());
+        }
+    }
+
+    public void handleButtonLoadFromFile() {
+
+    }
+
+    public void handleButtonLoadFromURL() {
+
     }
 }
